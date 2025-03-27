40858d3cb3dc7ca4e1a9059393667016fabf46e5
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-27 02:49:32 +0100

    Update git.nix

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,5 +1,5 @@
 let
-  name = "Aylur";
+  name = "xpr";
 in {
   programs.git = {
     enable = true;
@@ -10,7 +10,7 @@ in {
       github.user = name;
       push.autoSetupRemote = true;
     };
-    userEmail = "k.demeter@protonmail.com";
+    userEmail = " a.mameter@proton.me";
     userName = name;
   };
 }
